a7c6201cbcdc5ad3e0bd9b693f3965f27acbc96a
Gareth Coleman <gjc@rexprocess.com>
2020-08-19 04:53:13 +0100

    Added background map

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -1,118 +1,123 @@
-import { getCases, dataEndpoint, dataHost }   from '../service/getCases';
-import  React,{Component} from 'react'
+import { getCases, dataEndpoint, dataHost } from '../service/getCases';
+import React, { Component } from 'react'
 import areaData from '../service/areaPop'
-import {Location} from './location'
+import { Location } from './location'
 
 export class Dashboard extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.areaInfo = areaData;     
-    this.state={thedata:[],started:false};
+    this.areaInfo = areaData;
+    this.state = { thedata: [], started: false };
     this.getTheData = this.getTheData.bind(this);
     this.popData = areaData().sort();
     this.sortAreas = this.sortAreas.bind(this);
-    this.popDataObj ={};  
-    this.dataHost= dataHost;
-    this.thedates=[];
+    this.popDataObj = {};
+    this.dataHost = dataHost;
+    this.thedates = [];
     var date = new Date()
-    let daydiff= 11;
-    date.setDate(date.getDate()-daydiff);
-    this.startDate =  date;
-    while( daydiff> 4){
+    let daydiff = 11;
+    date.setDate(date.getDate() - daydiff);
+    this.startDate = date;
+    while (daydiff > 4) {
       daydiff--;
       date = new Date()
-      date.setDate(date.getDate()- daydiff);
-      this.thedates.push(date.toISOString().substr(0,10));  
+      date.setDate(date.getDate() - daydiff);
+      this.thedates.push(date.toISOString().substr(0, 10));
     }
-    this.endDate = date; 
-    this.popData.sort().map((area,index) => { area.index = index; area.currentrate=0 ; area.thisweekscases = 0 ; area.thisweeksrate=0; this.popDataObj[area.areaCode]= area});
+    this.endDate = date;
+    this.popData.sort().map((area, index) => { area.index = index; area.currentrate = 0; area.thisweekscases = 0; area.thisweeksrate = 0; this.popDataObj[area.areaCode] = area });
   }
 
-  componentDidMount()
-  {
-    if (!this.state.started){
-      this.setState({thedata:[],started:true});
-      this.thedates.map(thedate => {       
-             let dataEndpointForThisWeek = dataEndpoint.replace('{date}',thedate);
-             this.getTheData(dataHost + dataEndpointForThisWeek)
+  componentDidMount() {
+    if (!this.state.started) {
+      this.setState({ thedata: [], started: true });
+      this.thedates.map(thedate => {
+        let dataEndpointForThisWeek = dataEndpoint.replace('{date}', thedate);
+        this.getTheData(dataHost + dataEndpointForThisWeek)
       });
-    
+
     }
 
   }
 
-  sortAreas(area1,area2){
+  sortAreas(area1, area2) {
 
     //sort areas to be approximately north south, but with an east/west component
-    if ((area1.lat + (0.025*area1.long)) <(area2.lat+(0.025*area2.long)))
-    {
+    if ((area1.lat + (0.025 * area1.long)) < (area2.lat + (0.025 * area2.long))) {
       return 1
     }
-    else 
-    {
+    else {
       return -1
     }
   }
-  getTheData(url)
-  {
-    getCases(url).then(data =>{
+  getTheData(url) {
+    getCases(url).then(data => {
       var allData = this.state.thedata;
-      var i= 0;
+      var i = 0;
       let casedate;
-      data.data.map(value =>{
+      data.data.map(value => {
         try {
           value.rate = value.cases / (this.popDataObj[value.areaCode].pop / 100000);
           casedate = new Date(Date.parse(value.date));
-          if ( casedate >this.startDate && casedate<this.endDate)
-          {
-            if (value.cases>0){
-                this.popDataObj[value.areaCode].thisweekscases =  this.popDataObj[value.areaCode].thisweekscases + value.cases;              
+          if (casedate > this.startDate && casedate < this.endDate) {
+            if (value.cases > 0) {
+              this.popDataObj[value.areaCode].thisweekscases = this.popDataObj[value.areaCode].thisweekscases + value.cases;
             }
           }
         }
-        catch 
+        catch
         {
           console.log(value.areaCode)
         }
         allData.push(value);
-        });
+      });
 
-      this.popData.map(value=>   {
-        this.popDataObj[value.areaCode].currentrate = Math.round( this.popDataObj[value.areaCode].thisweekscases / (this.popDataObj[value.areaCode].pop/100000))
+      this.popData.map(value => {
+        this.popDataObj[value.areaCode].currentrate = Math.round(this.popDataObj[value.areaCode].thisweekscases / (this.popDataObj[value.areaCode].pop / 100000))
         value.currentrate = this.popDataObj[value.areaCode].currentrate;
         value.thisweekscases = this.popDataObj[value.areaCode].thisweekscases;
       })
 
-      this.setState({thedata:allData});
-      if ( allData.length<70000 && (typeof(data.pagination.next)!= 'undefined' && !!data.pagination.next))
-      {
-          this.getTheData(this.dataHost + data.pagination.next)
-      }  
-  
-    
+      this.setState({ thedata: allData });
+      if (allData.length < 70000 && (typeof (data.pagination.next) != 'undefined' && !!data.pagination.next)) {
+        this.getTheData(this.dataHost + data.pagination.next)
+      }
+
+
     });
-    
+
   }
 
-  render() {  
-    var  popData = this.popData;
+  render() {
+    var popData = this.popData;
+    var mapStyle= {
+      height:"1790px",
+      width:"1275px",
+      position:"absolute",
+      top:"-97px",
+      left:"0px",
+      zIndex:-1
+    }
 
+    return <div ><h3>Loading Regional Data {this.state.thedata.length} of about 3,000</h3>
+      <ul>
+        {popData.length < 1 ? <p>Not Yet</p> : popData.sort(this.sortAreas).map((value, index) => {
 
-  return <div ><h3>Loading Regional Data {this.state.thedata.length} of about 3,000</h3>
-    <ul >
-      {  popData.length <1 ? <p>Not Yet</p>: popData.sort(this.sortAreas).map((value, index) =>{ 
-            
-            return <Location  area={value.areaName} 
-                  rate={value.currentrate} 
-                  lat={value.lat} 
-                  long={value.long} 
-                  index={value.index} />
-            })}
-    </ul>
+          return <Location area={value.areaName}
+            rate={value.currentrate}
+            lat={value.lat}
+            long={value.long}
+            index={value.index} />
+        })}
+      </ul>
+      <img 
+    src={process.env.PUBLIC_URL + '/england.svg'} 
+    alt="map of england" 
+    
+    style={mapStyle} />
     </div>
-    ;
+      ;
   }
 }
-                    
-           
+
